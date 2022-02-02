fd18b8bd056c9a31bc05b5836437e1e8ae759428
Patusteg <patustegen@gmail.com>
2022-02-02 23:24:16 +0100

    rm

--- PROJECTE UF3/Jordisaac/Jordisaac/RoomManager.cpp
+++ PROJECTE UF3/Jordisaac/Jordisaac/RoomManager.cpp
@@ -1,7 +1,21 @@
 #include "RoomManager.h"
+#include <algorithm>
+#include <stdlib.h>     
+#include <time.h> 
+#include <math.h>
 
 RoomManager* RoomManager::pInstance = NULL;
 
+using namespace std;
+
 RoomManager::RoomManager()
 {
+	Level.resize(0);
+	aRoom = 0;
+	srand((unsigned int)time(NULL));
+}
+
+void RoomManager::createNewLevel(int lDiff)
+{
+	int maxRooms = min(20, (int)(rand() + 5 + floor(lDiff * 10 / 3)));
 }
