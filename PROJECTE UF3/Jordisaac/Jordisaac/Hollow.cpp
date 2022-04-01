b31b7212ddd838875c8d40d90b57121d04090746
Patusteg <patustegen@gmail.com>
2022-04-01 03:57:38 +0100

    Update Hollow.cpp

--- PROJECTE UF3/Jordisaac/Jordisaac/Hollow.cpp
+++ PROJECTE UF3/Jordisaac/Jordisaac/Hollow.cpp
@@ -34,18 +34,53 @@ void Hollow::update()
 	}
 	float moveX = 0, moveY = 0;
 
-	float vel = 1.5f * iVideo->getDeltaTime();
+	float vel = 0.35f * iVideo->getDeltaTime();
 	//VelX = Vel * cos(angle)
 	float velX = vel * cos(angle);
 
 	//VelY = Vel * sin(angle)
 	float velY = vel * sin(angle);
 
+	if (angle > 270 || angle < 90) col.restY -= velY;
+	else if (angle < 270 && angle > 90) col.restY += velY;
+	if (angle > 0 && angle < 180) col.restX += velX;
+	else if (angle < 360 && angle > 180) col.restX -= velX;
+	
 
 	col.restX = std::modf(col.restX, &moveX);
 	col.restY = std::modf(col.restY, &moveY);
 	col.x += moveX;
 	col.y += moveY;
+
+
+	if (col.x < ROOM_MARGIN_X)
+	{
+		col.x = ROOM_MARGIN_X;
+		angle = 360 - angle;
+	}
+	else if (col.x > SCREEN_WIDTH - ROOM_MARGIN_X - col.w) 
+	{
+		col.x = SCREEN_WIDTH - ROOM_MARGIN_X - col.w;
+		angle = 360 - angle;
+	}
+	if (col.y < ROOM_MARGIN_Y)
+	{
+		col.y = ROOM_MARGIN_Y;
+		angle = 180 - angle;
+	}
+	else if (col.y > SCREEN_HEIGHT - ROOM_MARGIN_Y - col.h)
+	{
+		col.y = SCREEN_HEIGHT - ROOM_MARGIN_Y - col.h;
+		angle = 180 - angle;
+	}
+
+
+	//> <
+
+	if (angle > 315 || angle < 45) orientacio = UP;
+	else if (angle <= 135 && angle >= 45) orientacio = RIGHT;
+	else if (angle < 225 && angle > 135) orientacio = DOWN;
+	else if (angle <= 315 && angle >= 225) orientacio = LEFT;
 
 	paint.x = col.x;
 	paint.y = col.y;
